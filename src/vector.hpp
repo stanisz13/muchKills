ca39fabc4d2378e563989279ed18b3cca0c49ca2
Grzegorz <g-kwacz@hotmail.com>
2018-07-22 01:48:11 +0200

    można mnożyć i dzielić vectory przez liczby

--- src/vector.hpp
+++ src/vector.hpp
@@ -46,6 +46,15 @@ template<typename T> struct vec2 {
         y = V.y;
     }
 
+    template<typename T2>
+    vec2<T> operator/ (T2 num) {
+        return {x/num, y/num};
+    }
+    template<typename T2>
+    vec2<T> operator* (T2 num) {
+        return {x*num, y*num};
+    }
+
     template<typename T2> void operator+= (T2 num) {
         x += num;
         y += num;
@@ -67,34 +76,29 @@ template<typename T> struct vec2 {
 
     template<typename T2>
     vec2<T> operator+ (const vec2<T2>& other) const {
-        T X = x + (T)other.x;
-        T Y = y + (T)other.y;
-        return vec2<T>{X, Y};
+        return vec2<T>{x + (T)other.x, y + (T)other.y};
     }
     template<typename T2>
     vec2<T> operator+= (const vec2<T2>& other) {
         x += (T)other.x;
         y += (T)other.y;
-
         return *this;
     }
     template<typename T2>
     vec2<T> operator- (const vec2<T2>& other) const {
-        T X = x - (T)other.x;
-        T Y = y - (T)other.y;
-        return vec2<T>{X, Y};
+        return vec2<T>{x - (T)other.x, y - (T)other.y};
     }
     template<typename T2>
     vec2<T> operator-= (const vec2<T2>& other) {
         x -= (T)other.x;
         y -= (T)other.y;
-
         return *this;
     }
 
     float lenght() const {
         return sqrt(x*x + y*y);
     }
+
     vec2<T> normalize() {
         float len = this->lenght();
         if(len == 0)
@@ -108,7 +112,6 @@ template<typename T> struct vec2 {
     operator sf::Vector2i() const {return {x, y};}
     operator sf::Vector2u() const {return {x, y};}
 };
-
 
 using vec2f = vec2<float>;
 using vec2i = vec2<float>;
