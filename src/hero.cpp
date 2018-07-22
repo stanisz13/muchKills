cf60d8e6af9f72d5dcd1fff9aa77ea7ceb1338d1
greg19 <g-kwacz@hotmail.com>
2018-07-22 12:55:42 +0200

    movement update

--- src/hero.cpp
+++ src/hero.cpp
@@ -4,13 +4,13 @@ bool Hero::handleMovement(const float& deltaTime) {
     vec2f direction;
 
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
-        direction -= vec2f{0, 1};
+        direction += vec2f{0, -1};
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-        direction -= vec2f{0, -1};
+        direction += vec2f{0, 1};
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-        direction -= vec2f{1, 0};
+        direction += vec2f{-1, 0};
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-        direction -= vec2f{-1, 0};
+        direction += vec2f{1, 0};
 
     if(direction.isZero())
         return 0;
