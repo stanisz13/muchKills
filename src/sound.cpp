db6838c659ffecdbfaa4a03036c4dc7a4dd705c7
Stanish <macios1999@gmail.com>
2018-07-22 13:34:18 +0200

    threaded sounds

--- src/sound.cpp
+++ src/sound.cpp
@@ -1,22 +1,19 @@
 #include <sound.hpp>
+#include <thread>
 
 void SoundManager::initSoundFiles()
 {
     Storage<sf::SoundBuffer>::add("hit", {}).loadFromFile("assets/hit.wav");
 }
 
-std::vector<sf::Sound> sounds;
+std::vector<std::thread> sounds;
 
 void SoundManager::play(const char* name)
 {
-    sf::Sound& sound = (sounds.emplace_back(), sounds.back());
-    sound.setBuffer(Storage<sf::SoundBuffer>::get(name));
-    sound.play();
-    if (sounds.size() >= 100)
-    {
-        std::vector<sf::Sound> newSounds;
-        for (unsigned i = sounds.size()- 1; i> sounds.size() - 11; --i)
-            newSounds.emplace_back(sounds[i]);
-        sounds = newSounds;
-    }
+    sounds.emplace_back([name](){
+        sf::Sound sound;
+        sound.setBuffer(Storage<sf::SoundBuffer>::get(name));
+        sound.play();
+        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    });
 }
