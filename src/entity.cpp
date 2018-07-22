f3bfa823b3cb8581849e1ea9fcde32bfb3168cff
greg19 <g-kwacz@hotmail.com>
2018-07-22 13:10:24 +0200

    Acceleration <3

--- src/entity.cpp
+++ src/entity.cpp
@@ -16,8 +16,14 @@ void Entity::setPos(const vec2f& newPos) {
 void Entity::move(const vec2f& deltaPos) {
     this->setPos(pos + deltaPos);
 }
+void Entity::accelerate(const vec2f& vec) {
+    acceleration += vec;
+}
+void Entity::deaccelerate(const float& deltaTime) {
+    acceleration *= pow(friction, deltaTime/deacceleration_time);
+}
 
-void Entity::update () {
+void Entity::update(const float& deltaTime) {
     auto bound = sprite.getGlobalBounds();
     sf::RectangleShape boxxx(vec2f(bound.width, bound.height));
     boxxx.setPosition(pos);
@@ -25,6 +31,8 @@ void Entity::update () {
     auto s = boxxx.getSize();
     box.setSize(vec2f(s.x, s.y));
 
+    this->move(acceleration);
+    this->deaccelerate(deltaTime);
 }
 void Entity::draw(sf::RenderWindow& window) {
     window.draw(sprite);
--- src/entity.hpp
+++ src/entity.hpp
@@ -11,13 +11,19 @@ struct Entity
     vec2f pos;
     bounding_box box;
 
+    vec2f acceleration;
+    float friction = 0.1f;
+    float deacceleration_time = 0.3f;
+
     Entity(){}
 
-    void setBox(vec2f pos, vec2f size);
+    void setBox(vec2f newPos, vec2f newSize);
 
     void setPos(const vec2f& newPos);
     void move(const vec2f& deltaPos);
+    void accelerate(const vec2f& vec);
+    void deaccelerate(const float& deltaTime);
 
-    void update();
+    void update(const float& deltaTime);
     void draw(sf::RenderWindow& window);
 };
