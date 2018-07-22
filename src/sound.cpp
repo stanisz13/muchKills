d79f8565e4ed99564280c4c740d2103aa2837f63
Stanish <macios1999@gmail.com>
2018-07-22 13:20:36 +0200

    maybe works?

--- src/sound.cpp
+++ src/sound.cpp
@@ -12,4 +12,11 @@ void SoundManager::play(const char* name)
     sf::Sound& sound = (sounds.emplace_back(), sounds.back());
     sound.setBuffer(Storage<sf::SoundBuffer>::get(name));
     sound.play();
+    if (sounds.size() >= 100)
+    {
+        std::vector<sf::Sound> newSounds;
+        for (unsigned i = sounds.size()- 1; i> sounds.size() - 11; --i)
+            newSounds.emplace_back(sounds[i]);
+        sounds = newSounds;
+    }
 }
